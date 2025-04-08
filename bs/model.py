c2687e33b5050f45500892da6ba9397ba6afa52f
ramybr <99534487+ramybr@users.noreply.github.com>
2025-04-08 13:52:20 +0100

    Fixes on model

--- bs/model.py
+++ bs/model.py
@@ -29,81 +29,46 @@ with M.rule():
     Categories.add(id=c.category_id).set(name=c.category_name)
 
 
-with M.rule():
-    cr = Customers
-    Customers.add(id=cr.customer_id).set(
-        first_name=cr.first_name,
-        last_name=cr.last_name,
-        phone=cr.phone,
-        email=cr.email,
-        street=cr.street,
-        city=cr.city,
-        state=cr.state,
-        zip_code=cr.zip_code
-    )
 
-with M.rule():
-    c = Customers()
-    o = Orders()
-    Orders.add(id=o.order_id).set(
-        customer_id=c.customer_id,
-        status=o.order_status,
-        order_date=o.order_date,
-        required_date=o.required_date,
-        shipped_date=o.shipped_date,
-        store_id=o.store_id,
-        staff_id=o.staff_id
-    )
-
-with M.rule():
-    p = Products()
-    p.add(id=p.product_id).set(
-        name=p.product_name,
-        brand_id=p.brand_id,
-        category_id=p.category_id,
-        model_year=p.model_yer,
-        listing_price=p.listing_price
-    )
-
-with M.rule():
-    s = Staff()
-    Staff.add(id=s.staff_id).set(
-         first_name=s.first_name,
-        last_name=s.last_name,
-        email=s.email,
-        phone=s.email,
-        active=s.active,
-        store_id=s.store_id,
-        manager_id=s.manager_id
-    )
 
 
 with M.rule():
-    st = Stores()
-    Stores.add(id=st.store_id).set(
-         name=st.store_name,
-        phone=st.phone,
-        email=st.email,
-        street=st.street,
-        city=st.city,
-        state=st.state,
-        zip_code=st.zip_code
-    )
+    prd = Products()
+    brnd = Brands(id=prd.brand_id)
+    ctg = Categories(id=prd.category_id)
+
+    prd.has_brand.set(brnd)
+    prd.has_category.set(ctg)
 
 with M.rule():
-    o = Orders()
+    ordr = Orders()
+    cst = Customers(id=ordr.customer_id)
+    st = Stores(id=ordr.store_id)
+    stf = Staff(id=ordr.staff_id)
+
+    ordr.has_customer.set(cst)
+    ordr.from_store.set(st)
+    ordr.has_staff.set(stf)
+
+    
+with M.rule():
+    stc = Stocks() 
+    st = Stores(id=stc.store_id)
+    pdct = Products(id=stc.product_id)
+
+    stc.in_store.set(st)
+    stc.has_product.set(pdct)
+
+with M.rule():
+    stf = Staff()
+    st = Stores(id=stf.store_id)
+
+    stf.works_in_store.set(st)
+
+with M.rule():
     oi = OrderItems()
-    OrderItems.add(item_id=oi.item_id, order_id=o.id).set(
-         product_id=oi.product_id,
-        quantity=oi.quantity,
-        list_price=oi.list_price,
-        discount=oi.discount
-    )
+    ordr = Orders(id=oi.order_id)
+    prd = Products(id=oi.product_id)
 
-with M.rule():
-    st = Stores()
-    p = Products()
-    stc = Stocks()
-    Stocks.add(store_id=st.id, product_id=p.id).set(quantity=stc.quantity)
-
-    # I'm not sure if that's the right way to express a join, i couldn't find a clear answer in the docs.
+    oi.in_order.set(ordr)
+    oi.has_product.set(prd)
